546ca65b70848848110d0375f1b8e1d097d8f902
jeanzb <jeanka20111@hotmail.com>
2026-06-04 01:23:10 -0500

    feat(collections): wire delete collection endpoint

--- apps/backend/src/NasaExplorer.API/Controllers/CollectionsController.cs
+++ apps/backend/src/NasaExplorer.API/Controllers/CollectionsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NasaExplorer.Application.DTOs.Collections;
 using NasaExplorer.Application.Features.Collections.Commands.CreateCollection;
+using NasaExplorer.Application.Features.Collections.Commands.DeleteCollection;
 using NasaExplorer.Application.Features.Collections.Commands.UpdateCollection;
 using NasaExplorer.Application.Features.Collections.Queries.GetCollectionById;
 using NasaExplorer.Application.Features.Collections.Queries.GetCollections;
@@ -52,6 +53,14 @@ public sealed class CollectionsController : ControllerBase
             cancellationToken);
 
         return Ok(result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(new DeleteCollectionCommand(id), cancellationToken);
+
+        return NoContent();
     }
 }
 
